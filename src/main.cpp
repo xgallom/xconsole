c51761004ef351e1258286f1994364c00441d5a4
xgallom <gallo.milan.jr@gmail.com>
2018-11-29 21:40:50 +0100

    tmp main
    Signed-off-by: xgallom <gallo.milan.jr@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,100 @@
  */
 
 #include "vga.h"
-#include "stm32f4xx.h"
+#include "usart.h"
+#include "gdi.h"
+#include "text.h"
+
+struct Position {
+	int x, y;
+};
+
+void printCharAt(Position position, char c)
+{
+
+}
 
 int main(void)
 {
-	SystemInit();
 	Vga::init();
+	Usart::init();
+
+	Gdi::border();
+
+	static const int BufferSize = Gdi::TextWidth * Gdi::TextHeight;
+	char buffer[BufferSize];
+	Position positions[BufferSize];
+
+	int bufferPosition = 0, bufferEnd = 0, x = 0, y = 0;
+
+	Gdi::printCharRow(x, y, '_');
+
+	bool escape = false;
+
+	while(bufferPosition < BufferSize) {
+		if(Usart::available()) {
+			char input = Usart::read();
+
+			Gdi::printCharRow(x, y, 0);
+
+			if(escape) {
+				if(input == 0x1b) {
+					bufferPosition = 0;
+					x = 0;
+					y = 0;
+
+					Gdi::cls();
+					Gdi::border();
+					escape = false;
+				}
+				else if(input == '[') {
+					while(!Usart::available()) {
+					}
+
+					input = Usart::read();
+				}
+			}
+			else {
+				buffer[bufferPosition] = input;
+
+				if(input == 0x1b) {
+					escape = true;
+				}
+				else if(input == 0x0d) {
+					positions[bufferPosition] = Position{ .x = x, .y = y };
+
+					x = 0;
+					++y;
+
+					++bufferPosition;
+				}
+				else if(input == '\b') {
+					if(bufferPosition) {
+						--bufferPosition;
+
+						buffer[bufferPosition] = 0x00;
+						x = positions[bufferPosition].x;
+						y = positions[bufferPosition].y;
+
+						Gdi::printCharRow(x, y, buffer[bufferPosition]);
+					}
+				}
+				else {
+					Gdi::printCharRow(x, y, buffer[bufferPosition]);
+					positions[bufferPosition] = Position{ .x = x, .y = y };
+
+					if(++x == Gdi::TextWidth) {
+						x = 0;
+						++y;
+					}
+
+					++bufferPosition;
+				}
+			}
+
+			Gdi::printCharRow(x, y, '_');
+		}
+	}
 
 	for (;;) {
 	}
